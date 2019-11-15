765b231eb466331f463bfd32952e487db1d8aac8
adityabajpai <adityabajpai0303@gmail.com>
2019-11-15 10:51:28 +0530

    Login and Register Form Done

--- src/Component/HeaderComponent.js
+++ src/Component/HeaderComponent.js
@@ -2,23 +2,56 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../Style/sideNav.css'
 import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import {ModalHeader, ModalBody, Modal, FormGroup, Label, Input} from 'reactstrap';
 
 class HeaderComponent extends React.Component{
 
     constructor(props){
         super(props);
+        this.state = {
+          isLoginModalOpen: false,
+          isRegisterModalOpen: false
+        };
         this.openSlideMenu = this.openSlideMenu.bind(this);
         this.closeSlideMenu = this.closeSlideMenu.bind(this);
+        this.toggleLoginModal = this.toggleLoginModal.bind(this);
+        this.toggleRegisterModal = this.toggleRegisterModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleRegister = this.handleRegister.bind(this);
+    }
+
+    toggleLoginModal(){
+        this.setState({
+            isLoginModalOpen: !this.state.isLoginModalOpen
+        })
+    }
+
+    handleLogin(event){
+        this.toggleLoginModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+    }
+
+    toggleRegisterModal(){
+        this.setState({
+            isRegisterModalOpen: !this.state.isRegisterModalOpen
+        })
+    }
+
+    handleRegister(event){
+        this.toggleRegisterModal();
+        alert("FirstName: " + this.firstName.value + " LastName: " + this.lastName.value + " Email: "+ this.email.value
+            + "password: " + this.password.value + " confirmPassword: " + this.confirmPassword.value+ " Remember: " + this.remember.checked);
+        event.preventDefault();
     }
 
     openSlideMenu(){
         document.getElementById('side-menu').style.width = '250px';
-        // document.getElementById('main').style.marginLeft = '250px';
     }
 
     closeSlideMenu(){
         document.getElementById('side-menu').style.width = '0px';
-        // document.getElementById('main').style.marginLeft = '0px';
     }
 
     render(){
@@ -56,27 +89,100 @@ class HeaderComponent extends React.Component{
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                        <Nav.Link href="#browseGames">Browse Cities</Nav.Link>
-                        <Nav.Link href="#gameJams">Browse Islands</Nav.Link>
-                        <Nav.Link href="#gameJams">Browse Forests</Nav.Link>
-                        <Nav.Link href="#gameJams">Browse Deserts</Nav.Link>
-                        <Nav.Link href="#uploadGame">Upload City/Island</Nav.Link>
-                        <Nav.Link href="#devlogs">Bloggers</Nav.Link>
-                        <Nav.Link href="#community">About Us</Nav.Link>
+                        <Nav.Link href="#exploreCities">Explore Cities</Nav.Link>
+                        <Nav.Link href="#exploreIslands">Explore Islands</Nav.Link>
+                        <Nav.Link href="#exploreForests">Explore Forests</Nav.Link>
+                        <Nav.Link href="#exploreDeserts">Explore Deserts</Nav.Link>
+                        <Nav.Link href="#uploadPlace">Upload Place</Nav.Link>
+                        <Nav.Link href="#bloggers">Bloggers</Nav.Link>
+                        <Nav.Link href="#aboutUs">About Us</Nav.Link>
                         </Nav>
                         <Form inline>
                         <FormControl type="text" placeholder="Search for places" className="mr-sm" />
                         <Nav>
                             <Nav.Link>
-                            <Button variant="outline-success">Login</Button>
+                            <Button variant="outline-success" onClick={this.toggleLoginModal}>Login</Button>
                             </Nav.Link>
                             <Nav.Link>
-                            <Button variant="outline-success">Register</Button>
+                            <Button variant="outline-success" onClick={this.toggleRegisterModal}>Register</Button>
                             </Nav.Link>
                         </Nav>
                         </Form>
                     </Navbar.Collapse>
                 </Navbar>
+                <Modal isOpen={this.state.isLoginModalOpen} toggle={this.toggleLoginModal}>
+                    <ModalHeader toggle={this.toggleLoginModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+
+
+                <Modal isOpen={this.state.isRegisterModalOpen} toggle={this.toggleRegisterModal}>
+                    <ModalHeader toggle={this.toggleRegisterModal}>Register</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleRegister}>
+                            <div className="row">
+                                <FormGroup className="col-6">
+                                    <Label htmlFor="firstName">FirstName</Label>
+                                    <Input type="text" id="firstName" name="firstName"
+                                    innerRef={(input) => this.firstName = input} />
+                                </FormGroup>
+                                <FormGroup className="col-6">
+                                    <Label htmlFor="lastName">LastName</Label>
+                                    <Input type="lastName" id="lastName" name="lastName"
+                                    innerRef={(input) => this.lastName = input}  />
+                                </FormGroup>
+                            </div>
+                            <div className="row">
+                                <FormGroup className="col-12 justify-content-center">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input type="email" id="email" name="email"
+                                    innerRef={(input) => this.email = input}/>
+                                </FormGroup>
+                            </div>
+                            <div className="row">
+                                <FormGroup className="col-6">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input} />
+                                </FormGroup>
+                                <FormGroup className="col-6">
+                                    <Label htmlFor="confirmPassword">Confirm Password</Label>
+                                    <Input type="password" id="confirmPassword" name="confirmPassword"
+                                    innerRef={(input) => this.confirmPassword = input}  />
+                                </FormGroup>
+                            </div>
+                            <FormGroup check>
+                                <Label check>
+                                <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Register</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
         )
     }
